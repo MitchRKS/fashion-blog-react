74fa0b096c45888f46163c4c725771ec2f1cae52
Mitch Rucker <mitchruckerks@gmail.com>
2023-02-23 11:58:54 -0600

    Replace Nav and Footer with components; Delete unused stock css

--- react-fashion-blog/src/App.jsx
+++ react-fashion-blog/src/App.jsx
@@ -3,15 +3,17 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Article } from "./Components";
 import { Header } from "./Components/Header";
+import { Nav } from "./Components";
+import { Footer } from "./Components";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h3>Nav Bar</h3>
+      <Nav />
       <Article />
-      <h5>Article</h5>
-      <h3>Footer</h3>
+      <Article />
+      <Footer />
     </div>
   );
 }
---
+++ react-fashion-blog/src/Components/Nav.jsx
@@ -0,0 +1,26 @@
+export function Nav() {
+  return (
+    <>
+      <ul aria-label="Main Navigation" role="navigation">
+        <li>
+          <a href="#">Women's</a>
+        </li>
+        <li>
+          <a href="#">Men's</a>
+        </li>
+        <li>
+          <a href="#">On the Street</a>
+        </li>
+        <li>
+          <a href="#">The Catwalk</a>
+        </li>
+        <li>
+          <a href="#">AdWatch</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </>
+  );
+}
